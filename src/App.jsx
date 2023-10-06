acdc791ec05a6a3b47d634c63cf48bb9ff635f4e
Marco <marcovaldes576@gmail.com>
2023-10-06 01:08:59 -0400

    Cambios de prueba

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,16 @@ function App() {
   return (
     <div className="flex">
       <div className="basis-[12%] h-[100vh] ">
+        {" "}
+        <h1>Cambios de pureba</h1>
         <Sidebar />
       </div>
       <div className="basis-[88%] border">
         <Dashboardview />
         <div>
-           <Outlet></Outlet>
-        </div>      
+          <Outlet></Outlet>
+        </div>
       </div>
-
     </div>
   );
 }
